6aa1f8ba863addb29b1070826ebcfd5ffcb1b624
bastionoffice <bastionoffice@gmail.com>
2017-03-25 12:59:36 -0400

    Clarified Comment

---
+++ Window_Available_time.bat
@@ -0,0 +1,4 @@
+@echo off
+REM just to set CMD window title to "Available" with time complete
+
+title Available %time:~0,8%
--- avail.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM just to set title to available when a batch file is done
-
-title Available %time:~0,8%
