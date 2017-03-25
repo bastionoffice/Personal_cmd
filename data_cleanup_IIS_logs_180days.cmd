a46d5c7f4aa756b237cf367583e6ad2e1b00b414
bastionoffice <bastionoffice@gmail.com>
2017-03-25 12:43:56 -0400

    using variables and cleaning commands
    Still needs the proper header

--- data_cleanup_IIS_logs_180days.cmd
+++ data_cleanup_IIS_logs_180days.cmd
@@ -1,11 +1,9 @@
-REM 11:02 AM 6/4/2012
+REM Updated 20170325
 REM cleanup script for the IIS Logs (services the Exchange pages)
 REM c:\admin\scripts\data_cleanup_IIS_logs_180days.cmd
+set logdir=g:\logs\IIS\W3SVC1
 
-pushd "pushd g:\logs\IIS\W3SVC1"
 REM remove log files older than 6 months
-forfiles /s /m *log /D -180 /c "cmd /c del @path"
+forfiles /p "%logdir%" /s /m *log /D -180 /c "cmd /c del @path"
 
-REM ::: not needed :::
-REM clean up empty folders recursively.
-REM for /f "usebackq delims=" %%d in (`"dir /ad/b/s | sort /R"`) do rd "%%d"
+
