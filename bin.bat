56ac3eaf2a0fdc758a18949187f5569006b4baf4
bastionoffice <bastionoffice@gmail.com>
2017-03-25 13:13:21 -0400

    Change directory to bin path
    update comments and default path to c:\admin\bin

--- bin.bat
+++ bin.bat
@@ -1,6 +1,9 @@
 @echo off
 @cls
-if "%computername%"=="GANDALF" (set target=C:\bin) else (set target=e:\bin)
+REM revised 20170325
+REM preferred convention is c:\admin\bin
+REM common at Catharsis
+if "%computername%"=="GANDALF" (set target=C:\bin) else (set target=c:admin\bin)
 if "%computername%"=="TOLKIEN" (set target=f:\bin) 
 
 echo current time is %time:~0,5%
