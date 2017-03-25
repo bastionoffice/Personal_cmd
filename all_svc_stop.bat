f9587cc9a43a7d2c5db67da69ad5e37112c37132
bastionoffice <bastionoffice@gmail.com>
2017-03-25 13:08:25 -0400

    Comment Clarity
    and a warning, and date change

--- all_svc_stop.bat
+++ all_svc_stop.bat
@@ -1,7 +1,8 @@
 
 REM	stop all services
-REM	10:21 PM 26/08/2006
-REM	no error handling here
+REM	26/08/2006
+REM	no error handling here, so beware being able to reverse this
+REM 	you may have to reboot
 	for /f "tokens=*" %%i in ('net start') do (net stop "%%i")
 			
 		
