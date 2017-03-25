5f32f09b7dce22acb9a74356262e56de49175053
bastionoffice <bastionoffice@gmail.com>
2017-03-25 12:56:49 -0400

    Clarified with comment

--- addhosts.bat
+++ addhosts.bat
@@ -1,3 +1,5 @@
 @echo off
+REM for adding pointers to localhost for unwanted domains
+REM usually advertising sites
 echo 127.0.0.1	%1 >> %systemroot%\system32\drivers\etc\hosts
 type %systemroot%\system32\drivers\etc\hosts
